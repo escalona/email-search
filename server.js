7b05bd1d84de9b4f82f4798700628fe51e572bf6
Aaron Escalona <escalona.ap@gmail.com>
2020-05-17 16:52:05 -0500

    Add search-index.json

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const Trie = require('./trie');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,24 +10,9 @@ app.set('view engine', 'hbs');
 app.use(express.static('dist'));
 app.use('/public', express.static(path.resolve(__dirname, 'public')));
 
-// server rendered home page
 app.get('/', async (req, res) => {
   const searchIndex = fs.readFileSync(path.join(__dirname, 'data/search-index.json'), 'utf8');
   res.render('index', { initialState: JSON.stringify(JSON.parse(searchIndex)) });
 });
 
-app.get('/index', async (req, res, next) => {
-  const trie = new Trie();
-
-  try {
-    await trie.indexEmails('./data/skilling-j');
-  } catch (err) {
-    console.error(err);
-    res.sendStatus(500);
-  }
-
-  res.json(trie);
-})
-;
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`Example app listening on port: ${port}`));
