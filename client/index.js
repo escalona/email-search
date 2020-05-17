2cabac8d6d02be971fd59adebc9fa39a92398b56
Aaron Escalona <escalona.ap@gmail.com>
2020-05-17 15:56:45 -0500

    Render search output results

--- client/index.js
+++ client/index.js
@@ -4,14 +4,14 @@ function search(word, trie) {
   var letters = word.split('');
 
   for (var i = 0; i < letters.length; i++) {
-    if (node.children[letters[i]]) {
-      node = node.children[letters[i]];
+    if (node.c[letters[i]]) {
+      node = node.c[letters[i]];
     } else {
       return output;
     }
 
     if (i === letters.length - 1) {
-      output = [...output, ...node.path];
+      output = [...output, ...node.p];
     }
   }
 
@@ -23,7 +23,21 @@ function handleSearchInput(e) {
   console.log(val);
   const output = search(val, window.__STATE__);
   console.log(output);
+  renderResults(output);
+}
+
+function renderResults(results) {
+  var template = '' +
+    '<div class="results-count">Found in <span' +
+    ' class="results-count-number">{{results.length}}</span>' +
+    ' files</div>\n' +
+    '{{#each results}}\n' +
+    '    <div class="result {{#if @first}}border-t-1{{/if}}">{{this}}</div>\n' +
+    '{{/each}}';
+  var templated = Handlebars.compile(template);
+  document.getElementById('results').innerHTML = templated({ results });
 }
 
 var searchEl = document.getElementById('search');
 searchEl.addEventListener('input', handleSearchInput);
+searchEl.focus();
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 const Trie = require('./trie');
 
@@ -10,16 +11,8 @@ app.use('/client', express.static(path.resolve(__dirname, 'client')));
 
 // server rendered home page
 app.get('/', async (req, res) => {
-  const trie = new Trie();
-
-  try {
-    await trie.indexEmails('./data/skilling-j');
-  } catch (err) {
-    console.error(err);
-    res.sendStatus(500);
-  }
-
-  res.render('index', { initialState: JSON.stringify(trie.toJSON()) });
+  const searchIndex = fs.readFileSync(path.join(__dirname, 'data/search-index.json'), 'utf8');
+  res.render('index', { initialState: JSON.stringify(JSON.parse(searchIndex)) });
 });
 
 app.get('/index', async (req, res, next) => {
